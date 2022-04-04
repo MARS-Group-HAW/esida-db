3e92bda9a296349aee4f5be229f2e37032663b52
Jonathan Ströbele <mail@jonathanstroebele.de>
2022-04-04 20:50:10 +0200

    cutting of n tiffs per parameter per shape/district

--- clip.sh
+++ clip.sh
@@ -4,14 +4,31 @@
 
 #gdalwarp -of GTiff -cutline districts_valid.shp -crop_to_cutline -csql "SELECT * FROM districts_valid WHERE District_N = '$id'" ./data/worldpop_pop/tza_ppp_2020_UNadj.tiff output_clip_${id}.tif
 
-clip_dir=./Districts
+clip_dir=./input/shapes/Districts
 
+
+# clip geotiffs
 for shp in "$clip_dir"/*.shp
 do
     filename=${shp##*/}
     basename="${filename%.shp}"
-    outdir="./output/$basename/worldpop/"
-    mkdir -p "$outdir"
 
-    gdalwarp -of GTiff -cutline "$shp" -crop_to_cutline ./data/worldpop_pop/tza_ppp_2020_UNadj.tiff "$outdir/tza_ppp_2020_UNadj.tiff"
+    tiffdirs=("worldpop_popc" "worldpop_pd" "worldpop_bsgme")
+
+    # loop over different geotiff features
+    for tiff in "${tiffdirs[@]}"
+    do
+        outdir="./output/$basename/$tiff/"
+        mkdir -p "$outdir"
+
+        # loop over each file (i.e. different years) in each feature
+        for tifffile in ./input/data/"$tiff"/*.tiff; do
+            outname=${tifffile##*/}
+
+            # only clip if target file does not exist
+            if [ ! -f "$outdir/$outname" ]; then
+                gdalwarp -of GTiff -cutline "$shp" -crop_to_cutline "$tifffile" "$outdir/$outname"
+            fi
+        done
+    done
 done
