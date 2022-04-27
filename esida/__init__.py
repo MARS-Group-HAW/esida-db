c3f0a711446b8cb5d3ec436f9a83592829897d9d
Jonathan Ströbele <mail@jonathanstroebele.de>
2022-04-27 09:55:50 +0200

    basic auth for whole app

--- esida/__init__.py
+++ esida/__init__.py
@@ -1,7 +1,9 @@
 import pkgutil
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_basicauth import BasicAuth
 
+from decouple import config
 from dbconf import get_conn_string
 
 
@@ -9,7 +11,14 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = get_conn_string()
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+app.config['BASIC_AUTH_USERNAME'] = config('FLASK_BASIC_AUTH_USERNAME', default='esida')
+app.config['BASIC_AUTH_PASSWORD'] = config('FLASK_BASIC_AUTH_FORCE', default='opendata22')
+app.config['BASIC_AUTH_FORCE'] = config('FLASK_BASIC_AUTH_FORCE', default=True, cast=bool)
+
+
 db = SQLAlchemy(app)
+basic_auth = BasicAuth(app)
+
 params  = [name for _, name, _ in pkgutil.iter_modules(['parameters'])]
 
 # import AFTER app, etc. has been declared!
