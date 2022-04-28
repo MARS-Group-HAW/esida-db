5c3d683bb694b9c3b036c3f5f6af4320c0f204a9
Jonathan Ströbele <mail@jonathanstroebele.de>
2022-04-28 18:15:45 +0200

    switch more stable lookin auth package

--- esida/__init__.py
+++ esida/__init__.py
@@ -1,7 +1,8 @@
 import pkgutil
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_basicauth import BasicAuth
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from decouple import config
 from dbconf import get_conn_string
@@ -11,13 +12,25 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = get_conn_string()
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.config['BASIC_AUTH_USERNAME'] = config('FLASK_BASIC_AUTH_USERNAME', default='esida')
-app.config['BASIC_AUTH_PASSWORD'] = config('FLASK_BASIC_AUTH_FORCE', default='opendata22')
-app.config['BASIC_AUTH_FORCE'] = config('FLASK_BASIC_AUTH_FORCE', default=True, cast=bool)
 
+users = {
+    config('FLASK_BASIC_AUTH_USERNAME', default='esida'): generate_password_hash(config('FLASK_BASIC_AUTH_PASSWORD', default='opendata22'))
+}
 
 db = SQLAlchemy(app)
-basic_auth = BasicAuth(app)
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+        check_password_hash(users.get(username), password):
+        return username
+
+if config('FLASK_BASIC_AUTH_FORCE', default=True, cast=bool):
+    @app.before_request
+    @auth.login_required
+    def require_login():
+        pass
 
 params  = [name for _, name, _ in pkgutil.iter_modules(['parameters'])]
 
