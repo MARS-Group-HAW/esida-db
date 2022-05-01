30191c1ac96f8d67973064e555fe61e7c62c3e71
Jonathan Ströbele <mail@jonathanstroebele.de>
2022-05-01 09:50:51 +0200

    fix: database gone error on first request

--- esida/__init__.py
+++ esida/__init__.py
@@ -9,9 +9,19 @@ from dbconf import get_conn_string
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = get_conn_string()
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# deployed app would often show an "sqlalchemy.exc.OperationalError:
+#   (psycopg2.OperationalError) server closed the connection unexpectedly"
+# exception on first request. This seems to be related to the connection
+# going idle. With the pool_pre_ping setting a simple SELECT 1; query is
+# used to test if the connection is still available for each new session.
+# see: https://stackoverflow.com/q/55457069/723769,
+# https://docs.sqlalchemy.org/en/14/core/pooling.html#pool-disconnects-pessimistic
+# https://github.com/pallets-eco/flask-sqlalchemy/issues/589
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {"pool_pre_ping": True}
+app.config['SQLALCHEMY_DATABASE_URI'] = get_conn_string()
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 users = {
     config('FLASK_BASIC_AUTH_USERNAME', default='esida'): generate_password_hash(config('FLASK_BASIC_AUTH_PASSWORD', default='opendata22'))
@@ -22,6 +32,7 @@ auth = HTTPBasicAuth()
 
 @auth.verify_password
 def verify_password(username, password):
+    """ Verify given password with known users. """
     if username in users and \
         check_password_hash(users.get(username), password):
         return username
@@ -30,6 +41,8 @@ if config('FLASK_BASIC_AUTH_FORCE', default=True, cast=bool):
     @app.before_request
     @auth.login_required
     def require_login():
+        """ this is a callback before ALL routes. by annotating it with
+        login_required we force ALL routes to be authenticated. """
         pass
 
 params  = [name for _, name, _ in pkgutil.iter_modules(['parameters'])]
