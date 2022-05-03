f36f99a6afc0667a09f9548a453e8e356c82390c
Jonathan Ströbele <mail@jonathanstroebele.de>
2022-05-03 15:38:34 +0200

    refactore: position of docblock

--- input/meta_data/documentation_in_md.py
+++ input/meta_data/documentation_in_md.py
@@ -1,16 +1,20 @@
-import numpy as np
-import pandas as pd
 """
 Script written by Kristopher Nolte as part of the ESIDA project om the 03.05.2022.
-Reads a meta file in form of a csv file and transform the information into markdown file for each parameter
+Reads a meta file in form of a csv file and transform the information into
+markdown file for each parameter.
 """
+
+import numpy as np
+import pandas as pd
+
 
 #Load the meta csv files and saves temporarily as dataframe
 df_dtype = pd.read_csv("DB_Meta_Sheet - Data Type & Processing.csv")
 df_dqual = pd.read_csv("DB_Meta_Sheet - Data Quality.csv")
 
 def check_for_match():
-    #checks if both list have identical entry. Raises Error if not else returns the list of abbreviations
+    """ Checks if both list have identical entry. Raises Error if not else
+    returns the list of abbreviations """
     abbrev1 = df_dtype["Abbreviation"]
     abbrev2 = df_dqual["Abbreviation"]
     if len(list(set(abbrev1) - set(abbrev2))) != 0:
@@ -19,13 +23,14 @@ def check_for_match():
         return abbrev1
 
 def documentation (abbrev, i):
-    #reads information from the description in the meta dataframe, transforms it into a markdown string
+    """ Reads information from the description in the meta dataframe, transforms
+    it into a markdown string """
     dtype_info = df_dtype.loc[df_dtype["Abbreviation"] == abbrev].T.rename(columns={i: "Description"}).dropna().to_markdown()
     dqual_info = df_dqual.loc[df_dqual["Abbreviation"] == abbrev].T.rename(columns={i: "Description"}).dropna().to_markdown()
     return dtype_info, dqual_info
 
 def write_md(abbrev, dtype_info, dqual_info):
-    #writes the markdown strings into a markdownfile and saves it
+    """ writes the markdown strings into a markdownfile and saves it """
     with open(".".join([abbrev, "md"]), 'w') as f:
         f.write("# Data type and processing information \n")
         f.write(dtype_info)
