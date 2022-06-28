b6fa95b8f1407d5ded204bbf83d32319941e7f38
Jonathan Ströbele <mail@jonathanstroebele.de>
2022-06-28 09:32:49 +0200

    print gunicorn logs to stdout so they show up in docker/k8s

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 # this file contains config params for gunicorn.
 # see: https://docs.gunicorn.org/en/stable/settings.html
 
-accesslog = "output/.logs/access.log"
-errorlog = "output/.logs/error.log"
+accesslog = "-"
+errorlog = "-"
 
 workers=3
