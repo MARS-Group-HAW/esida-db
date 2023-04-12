9f1d48a44a5f9e7b7203cc24275a1d11dddab151
Jonathan Ströbele <mail@jonathanstroebele.de>
2023-04-12 17:45:31 +0200

    feat: more age bin datalayers

---
+++ parameters/worldpop_age_15_49.py
@@ -0,0 +1,7 @@
+from esida.sexage_parameter import SexageParameter
+
+class worldpop_age_15_49(SexageParameter):
+
+    def __init__(self):
+        super().__init__()
+        self.ages = [15, 20, 25, 30, 35, 40, 45]
