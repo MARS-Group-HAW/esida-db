1861bbc0d5ef9cf419fb8ef526e24ce2505fd978
Jonathan Ströbele <mail@jonathanstroebele.de>
2023-05-25 14:18:30 +0200

    fix: show icons for parameter loading state

--- esida/static/js/app.js
+++ esida/static/js/app.js
@@ -1,6 +1,8 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 $(document).ready(function() {
     $('.js-datatable').DataTable({
