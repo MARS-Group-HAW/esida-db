4939846f78573c402b3dc47a0e0554337683d314
Jonathan Ströbele <mail@jonathanstroebele.de>
2023-06-05 16:16:36 +0200

    refactor: utilize caching data in DFs instead of query each time/shape value seperatly

--- esida/views/esida.py
+++ esida/views/esida.py
@@ -1,4 +1,5 @@
 import importlib
+import datetime as dt
 
 import pandas as pd
 from flask import render_template,request
@@ -20,10 +21,27 @@ def esida_risk_index():
     if not (dl_likelihood.is_loaded & dl_impact.is_loaded):
         return render_template('custom/esida_risk/warning.html.jinja')
 
+    whens = dl_impact.temporal_steps()
+    when = request.args.get('when', None)
+    if when:
+        dp = when.split('-')
+        if len(dp) == 3:
+            when = dt.date(year=int(dp[0]), month=int(dp[1]), day=int(dp[2]))
+        else:
+            when = whens[-1]
+    else:
+        when = whens[-1]
+
+    impact_df = dl_impact.download(when=when)
+    likelihood_df = dl_likelihood.download(when=when)
+
     for s in shapes:
         shape_type = s.type
-        impact     = s.get('esida_risk_impact')['esida_risk_impact']
-        likelihood = s.get('esida_risk_likelihood')['esida_risk_likelihood']
+
+        #impact     = s.get('esida_risk_impact')['esida_risk_impact']
+        #likelihood = s.get('esida_risk_likelihood')['esida_risk_likelihood']
+        impact     = int(impact_df[(impact_df['shape_id'] == s.id)]['esida_risk_impact'].iloc[0])
+        likelihood = int(likelihood_df[(likelihood_df['shape_id'] == s.id)]['esida_risk_likelihood'].iloc[0])
         label      = f"{s.name} {s.type}"
 
         if shape_type not in traces:
@@ -47,11 +65,13 @@ def esida_risk_index():
         traces[shape_type]['text'].append(label)
 
     return render_template('custom/esida_risk/index.html.jinja',
-                           shapes=shapes,
-                           dl_likelihood=dl_likelihood,
-                           dl_impact = dl_impact,
-                           traces=list(traces.values())
-                           )
+        shapes = shapes,
+        dl_likelihood = dl_likelihood,
+        dl_impact = dl_impact,
+        traces=list(traces.values()),
+        whens=whens,
+        when=when
+    )
 
 @app.route("/esida-risk/<int:shape_id>")
 def esida_risk_show(shape_id):
@@ -65,7 +85,6 @@ def esida_risk_show(shape_id):
         return render_template('custom/esida_risk/warning.html.jinja')
 
     shape = Shape.query.get(shape_id)
-
 
     # temporal
     likelihood_df = dl_likelihood.download(shape.id)
@@ -96,10 +115,25 @@ def esida_risk_show(shape_id):
     shapes = Shape.query.where(Shape.type == shape.type).all()
     traces = {}
 
+    whens = dl_impact.temporal_steps()
+    when = request.args.get('when', None)
+    if when:
+        dp = when.split('-')
+        if len(dp) == 3:
+            when = dt.date(year=int(dp[0]), month=int(dp[1]), day=int(dp[2]))
+        else:
+            when = whens[-1]
+    else:
+        when = whens[-1]
+
+    # create heatmap of all shapes in same category
+    impact_df = dl_impact.download(when=when)
+    likelihood_df = dl_likelihood.download(when=when)
+
     for s in shapes:
         shape_type = s.type
-        impact     = s.get('esida_risk_impact')['esida_risk_impact']
-        likelihood = s.get('esida_risk_likelihood')['esida_risk_likelihood']
+        impact     = int(impact_df[(impact_df['shape_id'] == s.id)]['esida_risk_impact'].iloc[0])
+        likelihood = int(likelihood_df[(likelihood_df['shape_id'] == s.id)]['esida_risk_likelihood'].iloc[0])
         label      = f"{s.name} {s.type}"
 
         if shape_type not in traces:
@@ -122,18 +156,15 @@ def esida_risk_show(shape_id):
         traces[shape_type]['y'].append(impact)
         traces[shape_type]['text'].append(label)
 
-    #traces['2'] = traces[shape_type].copy()
-    #traces['2']['type'] = "scatter"
-    #traces['2']['mode'] = "markers"
-
+    # Put actual shape on heatmap
     traces['shape'] = {
         'mode': 'markers+text',
         'type': 'scatter',
         'name': shape.name,
         'opacity': 1,
         'textposition': 'bottom center',
-        'x': [shape.get('esida_risk_likelihood')['esida_risk_likelihood']],
-        'y': [shape.get('esida_risk_impact')['esida_risk_impact']],
+        'x': [shape.get('esida_risk_likelihood', when=when)['esida_risk_likelihood']],
+        'y': [shape.get('esida_risk_impact', when=when)['esida_risk_impact']],
         'text': [],
         'marker': {
             'color': 'black',
@@ -141,37 +172,23 @@ def esida_risk_show(shape_id):
         }
     }
 
-
-    when_df = pd.read_sql('SELECT "when" FROM log_esida_risk_likelihood WHERE shape_id = %s ORDER BY index ASC',
+    # explain likelihood
+    log_likelihood_df = pd.read_sql('SELECT datalayer, current_score, threshold_score FROM log_esida_risk_likelihood WHERE shape_id = %s AND "when" = %s ORDER BY index ASC',
                      con=get_engine(),
-                     params=tuple([shape.id]));
-    when_df['when'] = when_df['when'].astype(str)
-    whens = list(when_df['when'].unique())
-    when = request.args.get('when', whens[-1])
+                     params=tuple([shape.id, when]))
 
-
-    #df = pd.read_csv('output/esida_risk_likelihood/esida_risk_likelihood.csv')
-    dfx = pd.read_sql('SELECT * FROM log_esida_risk_likelihood WHERE shape_id = %s AND "when" = %s ORDER BY index ASC',
-                     con=get_engine(),
-                     params=tuple([shape.id, when]));
-
-
-
-    #dfx = df[df['shape_id'] == shape.id]
-    #dfx = dfx[dfx['when'] == when]
-
-    traces2 = [{
-        'x': list(dfx['datalayer'].tolist()),
-        'y': list(dfx['current_score'].tolist()),
+    traces_likelihood = [{
+        'x': list(log_likelihood_df['datalayer'].tolist()),
+        'y': list(log_likelihood_df['current_score'].tolist()),
         'mode': 'lines+markers',
         'name': 'Score',
         'line': {
             'shape': 'hv'
         },
     }, {
-        'x': list(dfx['datalayer'].tolist()),
-        'y': list(dfx['threshold_score'].tolist()),
-        'text': list(dfx['threshold_score'].tolist()),
+        'x': list(log_likelihood_df['datalayer'].tolist()),
+        'y': list(log_likelihood_df['threshold_score'].tolist()),
+        'text': list(log_likelihood_df['threshold_score'].tolist()),
         'type': 'scatter',
         'name': 'Plus',
         'mode': 'markers+text',
@@ -181,27 +198,22 @@ def esida_risk_show(shape_id):
         },
     }]
 
-    #df = pd.read_csv('output/esida_risk_impact/esida_risk_impact.csv')
-
-    dfx = pd.read_sql('SELECT * FROM log_esida_risk_impact WHERE shape_id = %s AND "when" = %s ORDER BY index ASC',
+    # explain impact
+    log_impact_df = pd.read_sql('SELECT datalayer, current_score, threshold_score FROM log_esida_risk_impact WHERE shape_id = %s AND "when" = %s ORDER BY index ASC',
                      con=get_engine(),
                      params=tuple([shape.id, when]));
-
-    #dfx = df[df['shape_id'] == shape.id]
-    #dfx = dfx[dfx['when'] == when]
-
     traces_impact = [{
-        'x': list(dfx['datalayer'].tolist()),
-        'y': list(dfx['current_score'].tolist()),
+        'x': list(log_impact_df['datalayer'].tolist()),
+        'y': list(log_impact_df['current_score'].tolist()),
         'mode': 'lines+markers',
         'name': 'Score',
         'line': {
             'shape': 'hv'
         },
     }, {
-        'x': list(dfx['datalayer'].tolist()),
-        'y': list(dfx['threshold_score'].tolist()),
-        'text': list(dfx['threshold_score'].tolist()),
+        'x': list(log_impact_df['datalayer'].tolist()),
+        'y': list(log_impact_df['threshold_score'].tolist()),
+        'text': list(log_impact_df['threshold_score'].tolist()),
         'type': 'scatter',
         'name': 'Plus',
         'mode': 'markers+text',
@@ -215,7 +227,7 @@ def esida_risk_show(shape_id):
                            dl_likelihood=dl_likelihood,
                            dl_impact = dl_impact,
                            traces_temporal=traces_temporal,
-                           traces2=traces2,
+                           traces_likelihood=traces_likelihood,
                            traces_impact=traces_impact,
                            shape=shape,
                            whens=whens,
